021e494039daf9f2ed11312be69620cecf7e06a0
matheusFPZ <matheuspezzinifior@gmail.com>
2023-09-06 16:56:28 -0300

    modificacoes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -44,8 +44,43 @@ class Home extends StatelessWidget {
                     bottom: 20,
                     right: 20,
                     left: 20,
-                  )
-                ),)
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0,0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    ),]  ,
+                    borderRadius: BorderRadius.circular(10),
+
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'adicione aqui',
+                      border: InputBorder.none
+                    ),
+                  ),
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20
+
+                  ),
+                  child: ElevatedButton(
+                    child: Text ('+', style: TextStyle(fontSize: 40),),
+                    onPressed:() {},
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue,
+                      minimumSize: Size(60, 60)
+                    ),
+                    ),
+                  
+                )
               ]),
             )
           ],
